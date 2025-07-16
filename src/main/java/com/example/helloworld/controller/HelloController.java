69a21e2a0d035d3c655b8e130f803954df428676
GitHub Action <actions@github.com>
2025-07-16 16:42:42 +0900

    feat: 추가 작업사항3

--- src/main/java/com/example/helloworld/controller/HelloController.java
+++ src/main/java/com/example/helloworld/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.helloworld.Greeting;
 import com.example.helloworld.GreetingRepository;
 
-import lombok.RequiredArgsConstructor;
+
 
 @RestController
 // @RequiredArgsConstructor
